13c42f048e7caab544241c0f5797d2fb78f6e7c5
Frank J <frankj.legon@gmail.com>
2023-07-20 14:43:26 -0500

    Configuracion del proyecto y creacion de la app restaurant

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # LittleLemon
+
+## Crear proyecto
+
+```powershell
+> python3 -m venv venv # crear venv
+> source venv/bin/activate # activar venv
+> pip install -r requirements.txt # instalar librerias requeridas
+> pip install --upgrade pip # actualizar pip
+
+## ---- Projecto de django ----
+> django-admin startproject LittleLemon . # crear proyecto de django
+> python3 manage.py startapp Restaurant # Crear app Restaurant
+> python3 manage.py makemigrations # migraciones iniciales
+```
--- Restaurant/models.py
+++ Restaurant/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField(default=5)
+
+    def __str__(self) -> str:
+        return self.title
+
+class Reserva(models.Model):
+    name = models.CharField(max_length=255)
+    no_guest = models.IntegerField(default=6)
+    booking_date = models.DateTimeField()
+    
+    def __str__(self) -> str:
+        return self.name
